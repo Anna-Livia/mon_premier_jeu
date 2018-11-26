82f0a8fec9df74b561c4c3a167b41f2bde189bca
Anna-Livia <annalivia.gomart@gmail.com>
2018-11-26 21:23:40 +0100

    Initialise mon premier jeu

---
+++ mon_jeu.py
@@ -0,0 +1,22 @@
+import random
+target = random.randint(1, 100)
+print(target)
+
+still_playing = True
+while still_playing: 
+  print("Voulez vous jouer ?")
+  print ("Pressez A pour jouer, et Q pour quitter")
+  game_status = input()
+  if game_status in ["A", "a"]:
+    still_guessing = True
+    while still_guessing :
+      guess = int(input("Devine un chiffre "))
+      if guess < target : 
+        print("Trop petit")
+      elif guess > target : 
+        print("Trop grand")
+      else :
+        still_guessing = False
+        print("Bravo")
+  if game_status in ["Q", "q"]:
+    still_playing = False
